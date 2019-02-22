03c5883300a6f47b7237edb6617f528fd3bd354c
Jack Marston <jackdyett@gmail.com>
2019-02-22 15:05:05 -0700

    clean user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,38 +2,31 @@ class Api::V1::UsersController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
-    if current_user.location_permission
-      render json: geolocated_user
-    else
-      render json: current_user
-    end
+    render json: current_user
   end
 
   def update
     if params["latLng"]
-      render json: geolocated_user(params["latLng"])
-    else
+      current_user.update_attributes({ lat: params["latLng"]["lat"], lng: params["latLng"]["lng"] })
+    elsif params["allowLocation"]
       current_user.update_attribute(:location_permission, true)
-      render json: geolocated_user
+      geolocate_user_ip
     end
+
+    render json: current_user
   end
 
   private
 
-  def geolocated_user(lat_lng = nil)
+  def geolocate_user_ip
     @user = current_user
-    if lat_lng
-      @user.lat = lat_lng["lat"]
-      @user.lng = lat_lng["lng"]
-    else
       location_data = Geocoder.search(current_user.current_sign_in_ip).first.data
-      @user.lat = '%0.6f' % location_data["lat"]
-      @user.lng = '%0.6f' % location_data["lon"]
-    end
+      @user.update_attributes({
+        location_permission: true,
+        lat: '%0.6f' % location_data["lat"],
+        lng: '%0.6f' % location_data["lon"]
+      })
 
-    @user.save
-
-    return @user
   end
 
 end
